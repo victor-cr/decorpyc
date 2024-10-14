21cf454d602837719c1c42c0664b8f5d17321a6c
Viktor Polishchuk <victor2@ukr.net>
2024-10-14 13:30:51 +0300

    Updated project libraries Updated Ren`Py SDKs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,18 +3,18 @@ import sbt._
 object Dependencies {
   lazy val compileDependencies: Seq[ModuleID] = Seq(
     // Default logger API
-    "org.slf4j" % "slf4j-api" % "2.0.5" % Compile,
+    "org.slf4j" % "slf4j-api" % "2.0.12" % Compile,
     // Scala CLI API
-    "org.rogach" %% "scallop" % "5.0.0" % Compile,
+    "org.rogach" %% "scallop" % "5.1.0" % Compile,
   )
 
   lazy val runtimeDependencies: Seq[ModuleID] = Seq(
     // Logger backend
-    "ch.qos.logback" % "logback-classic" % "1.4.7" % Runtime
+    "ch.qos.logback" % "logback-classic" % "1.5.9" % Runtime
   )
 
   lazy val testDependencies: Seq[ModuleID] = Seq(
     // Test framework
-    "org.scalatest" %% "scalatest" % "3.2.16" % Test
+    "org.scalatest" %% "scalatest" % "3.2.19" % Test
   )
 }
